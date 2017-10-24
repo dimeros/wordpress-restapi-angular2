a277dae95e4a22ea4619d9c84c564daf5822f092
Dimeros <dimitris.imeros@gmail.com>
2017-10-24 23:02:47 +0300

    build url and post service structure...only json dato is coming!!

---
+++ wp-content/themes/ng-theme/source/services/UrlService.ts
@@ -0,0 +1,11 @@
+import {Injectable} from 'angular2/core';
+
+@Injectable()
+export class UrlService {
+   // private baseUrl: string = 'http://localhost/wp-angular2/wp-json/wp/v2';
+   private baseUrl: string = 'http://localhost/wordpress/wp-json/wp/v2';
+
+    constructor() {}
+
+    public buildUrl = (url: string) => this.baseUrl + url;
+}
